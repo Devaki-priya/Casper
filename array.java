46055d39cfbbb97c7eea2962e8ee215302c9de89
Devaki-priya <v.priya.bhuvi19@gmail.com>
2017-01-08 11:27:27 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class find largest smallest number{
+public static void main(string arg[])
+{
+int number[]= new int[]{32,43,53,54,32,65,63,98,43,23};
+int smallest[]=numbers[0];
+int largest[]=numbers[0];
+int(i=1;i<numbers.length;i++)
+{
+if (numbers[i]>largest)
+largest = numbers[i];
+else if(numbers[i]<smallest)
+smallest = numbers[i[;
+}
+system out println("largest number is:"+largest);
+system.out.println(smallest number is:"=smallest);
+}
+}
+
+
+
+
+
+
+
